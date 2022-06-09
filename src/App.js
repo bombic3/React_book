891cf59590a239b0d09fec96c120a5372bed90e8
bombi <bombic3@naver.com>
2022-06-09 16:57:37 +0900

    DOCS : 함수 컴포넌트와 클래스형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
 
-function App () {
-  const name = '리액트';
-  return (
-    <div className="react">
-      {name}
-    </div>
-  );
+// 클래스형 컴포넌트
+class App extends Component {
+  render () {
+    const name = 'react';
+    return <div className='react'>{name}</div>
+  }
 }
-// 리엑트에서 사용하는 코드 = JSX
-// JSX란 자바스크립트의 확장문법(XML과 비슷)
-// 반환되는 return안에서 두개의 요소이상은 div로 묶기
-// (리액트v16이상)<Fragment> or <> 사용
-// {} 안에 자바스크립트 문법 사용가능 if 안됨 조건부연산자(삼항연산자) 됨
-// () 괄호는 주로 JSX를 여러줄로 작성할 때 괄호로 감싸고, 한 줄로 표현할 수 있는 건 감싸지 않는다. 필수사항 아님.
-// undefined → {} 안에서 렌더링하는 것은 가능
-// style객체 미리 선언 후 style 값으로 지정
-// 바로 style 값 지정하기
 
+// 함수 컴포넌트
+// function App () {
+//   const name = '리액트';
+//   return (
+//     <div className="react">
+//       {name}
+//     </div>
+//   );
+// }
 
 export default App;
