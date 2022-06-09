33c9085089c97ad5d46697893397692a191d5b2b
bombi <bombic3@naver.com>
2022-06-09 17:06:52 +0900

    TEST : 첫 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import React, { Component } from 'react';
-import './App.css'
+import React from 'react';
+import MyComponent from './MyComponent';
 
-// 클래스형 컴포넌트
-class App extends Component {
-  render () {
-    const name = 'react';
-    return <div className='react'>{name}</div>
-  }
-}
-
-// 함수 컴포넌트
-// function App () {
-//   const name = '리액트';
-//   return (
-//     <div className="react">
-//       {name}
-//     </div>
-//   );
-// }
+const App = () => {
+  return (
+    <div>
+      <MyComponent />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,10 @@
+const MyComponent = () => {
+  return <div>나의 첫번째 컴포넌트!</div>
+}
+// function대신 화살표(arrow)함수 사용하여 간략하게 작성
+// 화살표 함수는 값을 연산하여 바로 반환해야 할 때 사용
+
+// 확장자 Reactjs Code Snippet 설치하면 rsc 엔터!
+// 클래스형은 rcc 엔터
+
+export default MyComponent;
