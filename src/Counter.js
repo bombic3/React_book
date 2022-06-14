310653057c2aa72bcdafad90551a815e94586123
bombi <bombic3@naver.com>
2022-06-14 17:59:58 +0900

    TEST : Counter - state안에 여러 값이 있을 때

--- src/Counter.js
+++ src/Counter.js
@@ -7,14 +7,16 @@ class Counter extends Component {
     super(props);
     // state의 초기값 설정
     this.state = {
-      number: 0
+      number: 0,
+      fixedNumber: 0
     };
   }
   render () {
-    const { number } = this.state; // state를 조회할 때는 this.state 로 조회
+    const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state 로 조회
     return (
       <div>
         <h1>{number}</h1>
+        <h2>바뀌지 않는 값: { fixedNumber }</h2>
         <button
           // onClick을 토어해 버튼이 클릭되었을 때 호출할 함수를 지정
           onClick={() => {
