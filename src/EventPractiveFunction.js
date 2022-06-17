4b1790b53a1b075ca72261c5746d693b7a97eb07
bombi <bombic3@naver.com>
2022-06-17 12:13:39 +0900

    TEST : 함수 컴포넌트에서 이벤트 사용시 useState를 통해 사용 상태에 객채 넣어보기

--- src/EventPractiveFunction.js
+++ src/EventPractiveFunction.js
@@ -1,16 +1,38 @@
 import React, { useState } from 'react';
 
+// useState를 통해 사용하는 상태에 문자열이 아닌 객체 넣어보기
+// 각각 input에 e.target.name을 활용하려면
+// useState를 쓸 때 인풋 값들이 들어 있는 form 객체 사용
+
 // 각각 input에 e.target.name을 활용하지 않고 onChange 관련 함수 두 개를 따로 만듦
 // input이 많으면 e.target.name을 활용
 const EventPractiveFunction = () => {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
-  const onChangeUsername = e => setUsername(e.target.value);
-  const onChangeMessage = e => setMessage(e.target.value);
+  const [form, setForm] = useState({
+    username: '',
+    message: ''
+  });
+  const { username, message } = form;
+  const onChange = e => {
+    const nextForm = {
+      ...form, // 스프레드 연산자(spread) 기존의 form 내용을 이자리에 복사한 뒤
+      [e.target.name]: e.target.value // 원하는 값 덮어 씌우기
+    };
+    setForm(nextForm);
+  }
+
+  // const [username, setUsername] = useState('');
+  // const [message, setMessage] = useState('');
+  // const onChangeUsername = e => setUsername(e.target.value);
+  // const onChangeMessage = e => setMessage(e.target.value);
+
   const onClick = () => {
     alert(username + ': ' + message);
-    setUsername('');
-    setMessage('');
+    // setUsername('');
+    // setMessage('');
+    setForm({
+      username: '',
+      message: ''
+    });
   };
   const onKeyPress = e => {
     if (e.key === 'Enter') {
@@ -20,20 +42,20 @@ const EventPractiveFunction = () => {
 
   return (
     <div>
-      <h1>함수 컴포넌트 이벤트 연습</h1>
+      <h1>함수 컴포넌트 이벤트 연습 - useState 사용하여 상태에 문자열 아닌 객체 넣어보기</h1>
       <input
           type="text"
           name="username"
           placeholder="사용자명 입력"
           value={username}
-          onChange={onChangeUsername}
+          onChange={onChange}
         />
         <input
           type="text"
           name="message"
           placeholder="아무거나 입력하세요"
           value={message}
-          onChange={onChangeMessage}
+          onChange={onChange}
           onKeyPress={onKeyPress}
         />
         <button onClick={onClick}>확인</button>
