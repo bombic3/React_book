abcafcc362816b1c2b529765f6caa981e5bec5fa
bombi <bombic3@naver.com>
2022-06-17 10:57:24 +0900

    TEST : 클래스형 컴포넌트에서 state에 input값 담아 띄우기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,29 +1,40 @@
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
+  state = {
+    message: ''
+  }
+
   render() {
     return (
       <div>
-        <h1>이벤트 연습</h1>
+        <h1>클래스형 컴포넌트 이벤트 연습 - state에 input값 담아 띄우기</h1>
+        <p>생성자 메서드인 constructor에서 state 초깃값을 설정하고</p>
+        <p>이벤트 핸들링 함수 내부에서 this.setState 메서드를 호출하여 state를 업데이트 해보기</p>
+        <p>그 다음 input의 value 값을 state에 있는 값으로 설정</p>
         <input
           type="text"
           name="message"
           placeholder="아무거나 입력하세요"
+          value={this.state.message}
           onChange={
             (e) => {
-              // console.log(e);
-              console.log(e.target.value);
+              this.setState({
+                message: e.target.value
+              })
             }
-            // 콘솔에 기록되는 e 객체는 SyntheticEvent(합성이벤트)로
-            // 웹 브라우저의 네이티브 이벤트를 감싸는 객체이다.
-            // 네이티브 이벤트와 인터페이스가 같으므로
-            // 순수 자바스크립트에서 HTML 이벤트를 다룰 때와 똑같이 사용하면 됩니다.
-            // SyntheticEvent는 네이티브 이벤트와 달리 이벤트가 끝나고 나면
-            // 이벤트가 초기화 되므로 정보를 참조 할 수 없다.
-            //  ex) 0.5초 뒤에 e객체를 참조하면 e객체 내부의 모든 값이 비워짐
-            // 만약 비동기적으로 이벤트 객체를 참조할 일이 있다면 e.persist() 함수를 호출해야 함
           }
         />
+        {/* 입력한 값 state에 넣기 */}
+        <button onClick={
+          () => {
+            alert(this.state.message);
+            this.setState({
+              message: ''
+            });
+          }
+        }>확인</button>
+        {/* state에 넣은 값 띄우기 */}
       </div>
     );
   }
